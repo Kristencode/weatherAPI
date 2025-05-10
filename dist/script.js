c78242ae1b70169d8e00aca743007c2b9339733b
Kristen <harunakristen@gmail.com>
2025-05-10 18:51:31 +0000

    more update on consumption...

--- dist/script.js
+++ dist/script.js
@@ -12,7 +12,8 @@ formContainer.addEventListener("submit", function (event) {
 
 // Fetching API starts here
 function getCurrentWeather(city) {
-  let fetchedApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${secretKey}&units=metric`;
+  // the url + the key/city..assigned the url to a new variable
+  let fetchedApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${secretKey}&units=metric`; 
 
   fetch(fetchedApi)
     .then(function (res) {
@@ -24,5 +25,34 @@ function getCurrentWeather(city) {
     .catch(function (err) {
       console.error("Error:", err);
     });
+}
+
+// if the city isn't found or data doesnt read 200 which is 'OK' or 'success' it returns
+function updateUI(data) {
+  if (!data || data.cod !== 200) {
+    alert("City not found!");
+    return;
+  }
+
+  // the text data.name is the nme of the city. cities searched will be returned as capital letter
+  document.querySelector("h1").textContent = data.name.toUpperCase();
+  document.querySelector("#weatherMain").textContent =
+    data.weather[0].main;
+  document.querySelector("#Maintemp").innerHTML = `${Math.round(
+    data.main.temp
+  )}&#176;`;
+
+  const iconCode = data.weather[0].icon;
+  document.querySelector(
+    "img"
+  ).src = `http://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+  // Update min/max
+  document.querySelectorAll(
+    ".flex.justify-center p"
+  )[0].innerHTML = `Min ${Math.round(data.main.temp_min)}&#176;`;
+  document.querySelectorAll(
+    ".flex.justify-center p"
+  )[1].innerHTML = `Max ${Math.round(data.main.temp_max)}&#176;`;
 }
 
